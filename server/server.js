01845c9f80f92509a9045170224eae61893b794c
Vitalii <acht-acht@mail.ru>
2016-06-02 20:49:31 +0300

    server.js fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static('../public'));
+app.use(express.static('./public'));
 
 app.listen(PORT, function(){
     console.log("Server running on " + PORT);
