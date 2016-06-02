a23fa65fc17371cab851d6208c21be7d63052e89
Vitalii <acht-acht@mail.ru>
2016-06-02 20:22:18 +0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('../public'));
+
+app.listen(PORT, function(){
+    console.log("Server running on " + PORT);
+});
